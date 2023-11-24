43d878bc17ec0e5c7bb332cbd4704b74d85c754e
hosu kang <rkdghtn6036@gmail.com>
2023-11-24 15:18:50 +0900

    강호수는 할일을 다 하였다. 우하하 (최종)

--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,181 +1,61 @@
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Routes, Route, useParams, useNavigate } from 'react-router-dom';
-// import { v4 as uuidv4 } from 'uuid';
-
-// const PostDetail = ({ post }) => {
-//   const [comment, setComment] = useState('');
-//   const [comments, setComments] = useState([]);
-//   const navigate = useNavigate();
-
-//   const handleComment = (e) => {
-//     e.preventDefault();
-//     setComments([comment, ...comments]);
-//     setComment('');
-//   };
-
-//   return (
-//     <div>
-//       <h2>{post.nickname}</h2>
-//       <p>작성자: {post.userId}</p>
-//       <p>작성 시간: {post.createdAt}</p>
-//       {Object.values(post.content).map((item, i) => (
-//         <div key={i}>{item}</div>
-//       ))}
-//       <p>조회수: {post.views}</p>
-//       <p>좋아요 수: {post.likes}</p>
-//       <h3>댓글</h3>
-//       {comments.map((comment, i) => (
-//         <p key={i}>{comment}</p>
-//       ))}
-//       <form onSubmit={handleComment}>
-//         <input
-//           type="text"
-//           value={comment}
-//           onChange={(e) => setComment(e.target.value)}
-//           placeholder="댓글을 입력하세요."
-//         />
-//         <button type="submit">댓글 달기</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// const Main = () => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(true); // 로그인 상태 나타냄
-//   const [posts, setPosts] = useState([]); //게시글
-//   const [input, setInput] = useState({
-//     oneThank: '',
-//     twoThank: '',
-//     threeThank: '',
-//     fourThank: '',
-//     fiveThank: ''
-//   }); // 5감사
-//   const navigate = useNavigate();
-//   const handleInput = (event) => {
-//     setInput({
-//       ...input,
-//       [event.target.name]: event.target.value
-//     });
-//   };
-//   //게시하기 버튼 클릭
-//   const handleSubmit = () => {
-//     if (isLoggedIn && Object.values(input).every((item) => item !== '')) {
-//       const newPost = {
-//         userId: 'user123',
-//         nickname: 'nickname123',
-//         createdAt: new Date().toLocaleString(),
-//         content: input,
-//         views: 0,
-//         likes: 0
-//       };
-//       setPosts([newPost, ...posts]); // 게시글 추가
-//       setInput({
-//         oneThank: '',
-//         twoThank: '',
-//         threeThank: '',
-//         fourThank: '',
-//         fiveThank: ''
-//       }); //게시글 초기화
-//     } else {
-//       alert('로그인이 필요하거나, 감사한 사항을 5가지 모두 입력해주세요.');
-//     }
-//   };
-//   // 게시글 클릭시 조회수 증가 및 상세 페이지로 이동
-//   const handleView = (index) => {
-//     const newPosts = [...posts];
-//     newPosts[index].views += 1;
-//     setPosts(newPosts);
-//     navigate(`/detail/${index}`); // 해당 post의 상세 페이지로 이동
-//   };
-//   //좋아요 클릭시 숫자 증가
-//   const handleLike = (event, index) => {
-//     event.stopPropagation(); // 게시글 좋아요 수 더하기 방지
-//     const newPosts = [...posts];
-//     newPosts[index].likes += 1;
-//     setPosts(newPosts);
-//   };
-
-//   return (
-//     <main>
-//       <div>5감사사 설명</div>
-//       <div>
-//         <input
-//           type="text"
-//           name="oneThank"
-//           value={input.oneThank}
-//           onChange={handleInput}
-//           placeholder="첫 번째 감사한 사항을 입력하세요."
-//         />
-//         <input
-//           type="text"
-//           name="twoThank"
-//           value={input.twoThank}
-//           onChange={handleInput}
-//           placeholder="두 번째 감사한 사항을 입력하세요."
-//         />
-//         <input
-//           type="text"
-//           name="threeThank"
-//           value={input.threeThank}
-//           onChange={handleInput}
-//           placeholder="세 번째 감사한 사항을 입력하세요."
-//         />
-//         <input
-//           type="text"
-//           name="fourThank"
-//           value={input.fourThank}
-//           onChange={handleInput}
-//           placeholder="네 번째 감사한 사항을 입력하세요."
-//         />
-//         <input
-//           type="text"
-//           name="fiveThank"
-//           value={input.fiveThank}
-//           onChange={handleInput}
-//           placeholder="다섯 번째 감사한 사항을 입력하세요."
-//         />
-//         <button onClick={handleSubmit}>게시</button>
-//       </div>
-//       <div>게시글 검색 </div>
-//       <div>게시글 필터링</div>
-//       <div>
-//         {posts.map((post, index) => (
-//           <div key={index} onClick={() => handleView(index)}>
-//             작성자: {post.userId} / 닉네임: {post.nickname}
-//             작성 시간: {post.createdAt}
-//             {Object.values(post.content).map((item, i) => (
-//               <div key={i}>{item}</div>
-//             ))}
-//             조회수: {post.views}
-//             좋아요 수: {post.likes}
-//             <button onClick={(event) => handleLike(event, index)}>좋아요</button>
-//           </div>
-//         ))}
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default Main;
 // Main.jsx
-import React, { useState } from 'react';
+// React와 필요한 hook들을 불러옵니다.
+import React, { useState, useEffect } from 'react';
+// react-router-dom에서 페이지 이동을 위한 hook을 불러옵니다.
 import { useNavigate } from 'react-router-dom';
+// Firebase 설정과 Firestore에서 필요한 함수들을 불러옵니다.
 import { db, arrayUnion } from '../../firebase';
-import { collection, addDoc, doc, updateDoc } from 'firebase/firestore';
+import { collection, addDoc, doc, updateDoc, query, orderBy, where } from 'firebase/firestore';
 import { increment } from 'firebase/firestore';
 import { setDoc } from 'firebase/firestore';
+import { getDocs, deleteDoc } from 'firebase/firestore';
 
+// Main 컴포넌트를 정의합니다.
 const Main = () => {
-  const [posts, setPosts] = useState([]); //게시글
+  // state를 정의합니다. posts는 게시글을 저장하고, input은 감사 내용을 저장합니다.
+  const [posts, setPosts] = useState([]);
   const [input, setInput] = useState({
     oneThank: '',
     twoThank: '',
     threeThank: '',
     fourThank: '',
     fiveThank: ''
-  }); // 5감사
+  });
+  const [searchName, setSearchName] = useState('');
+  const [searchNickname, setSearchNickname] = useState('');
+  const [filter, setFilter] = useState('latest');
+
+  // 페이지 이동을 위한 hook을 초기화합니다.
   const navigate = useNavigate();
 
+  // 컴포넌트가 렌더링될 때 게시글을 Firestore에서 불러옵니다.
+  useEffect(() => {
+    const fetchPosts = async () => {
+      let orderByField = 'createdAt';
+      let orderDirection = 'desc';
+      if (filter === 'oldest') {
+        orderDirection = 'asc';
+      } else if (filter === 'views' || filter === 'likes' || filter === 'comments') {
+        orderByField = filter;
+      }
+
+      let postQuery = query(collection(db, 'posts'), orderBy(orderByField, orderDirection));
+      if (searchName) {
+        postQuery = query(postQuery, where('userId', '==', searchName));
+      }
+      if (searchNickname) {
+        postQuery = query(postQuery, where('nickname', '==', searchNickname));
+      }
+
+      const postSnapshot = await getDocs(postQuery);
+      const postList = postSnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setPosts(postList);
+    };
+
+    fetchPosts();
+  }, [filter, searchName, searchNickname]);
+
+  // input의 변경 사항을 처리하는 함수입니다.
   const handleInput = (event) => {
     setInput({
       ...input,
@@ -183,12 +63,15 @@ const Main = () => {
     });
   };
 
+  // 게시 버튼을 눌렀을 때 실행되는 함수입니다.
   const handleSubmit = () => {
+    // input이 모두 채워져 있는지 확인합니다.
     if (!Object.values(input).every((item) => item.trim() !== '')) {
       alert('감사한 사항을 5가지 모두 입력해주세요.');
       return;
     }
 
+    // 새 게시글 객체를 만듭니다.
     const newPost = {
       userId: 'user123',
       nickname: 'nickname123',
@@ -199,15 +82,18 @@ const Main = () => {
       comments: []
     };
 
+    // Firestore에 새 문서를 추가합니다.
     addDoc(collection(db, 'posts'), newPost)
       .then((docRef) => {
         console.log('Document written with ID: ', docRef.id);
-        setPosts([{ id: docRef.id, ...newPost }, ...posts]); // Firestore에서 생성된 ID를 저장
+        // Firestore에서 생성한 문서 id를 저장하고 posts state를 업데이트합니다.
+        setPosts([...posts, { id: docRef.id, ...newPost }]);
       })
       .catch((error) => {
         console.error('Error adding document: ', error);
       });
 
+    // input을 초기화합니다.
     setInput({
       oneThank: '',
       twoThank: '',
@@ -217,22 +103,39 @@ const Main = () => {
     });
   };
 
-  const handleView = async (index) => {
-    const docRef = doc(db, 'posts', posts[index].id); // 저장해 둔 ID를 사용
+  // 게시글을 클릭했을 때 조회수를 증가시키고 상세 페이지로 이동하는 함수입니다.
+  const handleView = async (postId) => {
+    const docRef = doc(db, 'posts', postId);
     await updateDoc(docRef, {
       views: increment(1)
     });
-    navigate(`/detail/${index}`);
+    navigate(`/detail/${postId}`);
   };
+  // 좋아요 버튼을 눌렀을 때 좋아요 수를 증가시키는 함수입니다.
+  const handleLike = async (event, postId, index) => {
+    event.stopPropagation();
+    const docRef = doc(db, 'posts', postId);
+    await updateDoc(docRef, {
+      likes: increment(1)
+    });
 
-  //좋아요 클릭시 숫자 증가
-  const handleLike = (event, index) => {
-    event.stopPropagation(); // 게시글 좋아요 수 더하기 방지
     const newPosts = [...posts];
     newPosts[index].likes += 1;
     setPosts(newPosts);
   };
+  // 수정 버튼을 눌렀을 때 상세 페이지로 이동하는 함수입니다.
+  const handleEdit = (id) => {
+    navigate(`/detail/${id}`);
+  };
 
+  // 삭제 버튼을 눌렀을 때 해당 게시글을 삭제하는 함수입니다.
+  const handleDelete = async (id) => {
+    const docRef = doc(db, 'posts', id);
+    await deleteDoc(docRef);
+    setPosts(posts.filter((post) => post.id !== id));
+  };
+
+  // 렌더링합니다. 여기서는 감사 내용 입력란, 게시글 검색 및 필터링, 게시글 목록을 표시합니다.
   return (
     <main>
       <div>5감사사 설명</div>
@@ -274,17 +177,30 @@ const Main = () => {
         />
         <button onClick={handleSubmit}>게시</button>
       </div>
-      <div>게시글 검색 </div>
+
       <div>게시글 필터링</div>
       <div>
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="latest">최신순</option>
+          <option value="oldest">오래된순</option>
+          <option value="views">조회순</option>
+          <option value="likes">좋아요순</option>
+          <option value="comments">댓글순</option>
+        </select>
+      </div>
+
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '20px' }}>
         {posts.map((post, index) => (
-          <div key={index} onClick={() => handleView(index)}>
+          <div key={index} onClick={() => handleView(post.id)} style={{ border: '1px solid black', padding: '10px' }}>
             작성자: {post.userId} / 닉네임: {post.nickname}
             작성 시간: {post.createdAt}
-            {post.content && Object.values(post.content).map((item, i) => <div key={i}>{item}</div>)}
+            {post.content &&
+              ['oneThank', 'twoThank', 'threeThank', 'fourThank', 'fiveThank'].map((key, i) => (
+                <div key={i}>{post.content[key]}</div>
+              ))}
             조회수: {post.views}
             좋아요 수: {post.likes}
-            <button onClick={(event) => handleLike(event, index)}>좋아요</button>
+            <button onClick={(event) => handleLike(event, post.id, index)}>좋아요</button>
           </div>
         ))}
       </div>
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,15 +1,30 @@
 // Detail.jsx
+// React와 필요한 hook들을 불러옵니다.
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+// react-router-dom에서 페이지 이동 및 파라미터를 가져오는 hook을 불러옵니다.
+import { useParams, useNavigate } from 'react-router-dom';
+// Firebase 설정과 Firestore에서 필요한 함수들을 불러옵니다.
 import { db, arrayUnion } from '../firebase';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc, updateDoc } from 'firebase/firestore';
+import { increment } from 'firebase/firestore';
 
+// Detail 컴포넌트를 정의합니다.
 const Detail = () => {
+  // state를 정의합니다. post는 게시글을, comment는 댓글을, comments는 댓글 목록을 저장합니다.
   const [post, setPost] = useState(null);
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
+  // URL 파라미터에서 id를 가져옵니다.
   const { id } = useParams();
+  // 페이지 이동을 위한 hook을 초기화합니다.
+  const navigate = useNavigate();
 
+  // 홈으로 돌아가는 함수입니다.
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
+  // 컴포넌트가 렌더링될 때 게시글을 Firestore에서 불러옵니다.
   useEffect(() => {
     const fetchPost = async () => {
       const docRef = doc(db, 'posts', id);
@@ -26,29 +41,44 @@ const Detail = () => {
     fetchPost();
   }, [id]);
 
+  // 게시글이 아직 불러와지지 않았다면 로딩 메시지를 표시합니다.
   if (!post) return <div>Loading...</div>;
 
+  // 좋아요 버튼을 눌렀을 때 실행되는 함수입니다.
+  const handleLike = () => {
+    const docRef = doc(db, 'posts', id);
+    updateDoc(docRef, {
+      likes: increment(1)
+    }).then(() => {
+      setPost((prevPost) => ({ ...prevPost, likes: prevPost.likes + 1 }));
+    });
+  };
+
+  // 댓글을 작성할 때 실행되는 함수입니다.
   const handleComment = (e) => {
     e.preventDefault();
     const docRef = doc(db, 'posts', id);
     const newComment = {
       text: comment,
-      userId: 'user123', // 현재 로그인한 사용자의 ID
-      createdAt: new Date().toLocaleString() // 현재 시간
+      userId: 'user123',
+      createdAt: new Date().toLocaleString()
     };
-    setDoc(docRef, { comments: arrayUnion(newComment) }, { merge: true })
+    updateDoc(docRef, {
+      comments: arrayUnion(newComment)
+    })
       .then(() => {
-        console.log('Document successfully updated!');
-        setComments([newComment, ...comments]);
+        setComments((prevComments) => [newComment, ...prevComments]);
+        setComment('');
       })
       .catch((error) => {
         console.error('Error updating document: ', error);
       });
-    setComment('');
   };
 
+  // 렌더링합니다. 여기서는 게시글 내용, 좋아요 버튼, 댓글 기능 등을 표시합니다.
   return (
     <div>
+      <button onClick={handleGoHome}>홈으로 가기</button>
       <h2>{post.nickname}</h2>
       <p>작성자: {post.userId}</p>
       <p>작성 시간: {post.createdAt}</p>
